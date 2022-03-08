ae7b244ba68a0a2c0cddb4fc2cc2fe02b39c6f29
Mosaab Laboune <51124222+mosaablaboune@users.noreply.github.com>
2022-03-08 22:04:51 +0100

    Adding views and vue-router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from '../views/Home.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    linkActiveClass: 'active'
+})
+
+
+export default router;
